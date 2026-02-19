588011f2baf53687349f94c6fc966416870c36b5
Yoogi <yoogikov@gmail.com>
2026-02-19 13:38:01 +0530

    added mason

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -49,6 +49,25 @@ end
 
 return {
   {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "rust_analyzer",
+          "clangd",
+        }
+      })
+    end
+  },
+  {
     "neovim/nvim-lspconfig",
     dependencies = {
       {
